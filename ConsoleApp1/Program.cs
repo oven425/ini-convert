ab53c632563e509fe9af738c9f5964faa678e8c2
oven425 <oven425@yahoo.com.tw>
2024-05-23 10:26:57 +0800

    update
    1. study source generate

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+[Generator]
+public class CustomGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(static postInitializationContext => {
+            postInitializationContext.AddSource("myGeneratedFile.cs", SourceText.From("""
+                using System;
+
+                namespace GeneratedNamespace
+                {
+                    internal sealed class GeneratedAttribute : Attribute
+                    {
+                    }
+                }
+                """, Encoding.UTF8));
+        });
+    }
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp2
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFrom("Generated Code");
+        }
+
+        static partial void HelloFrom(string name);
+    }
+}
