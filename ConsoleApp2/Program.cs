9c56c96eb97942f349a3beda29e34d59f44687d2
oven425 <oven425@yahoo.com.tw>
2024-06-11 20:55:32 +0800

    update
    1. study source generate

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,10 +5,41 @@
         static void Main(string[] args)
         {
             HelloFrom("Generated Code");
-
+            AugmentClass a = new AugmentClass();
+            a.GeneratedMethod();
             Console.ReadLine();
+
+            
         }
 
         static partial void HelloFrom(string name);
+    }
+
+    public partial class AugmentClass
+    {
+        public void AugmentMethod()
+        {
+            // 调用代码生成器中的方法
+            this.GeneratedMethod();
+        }
+    }
+
+    [CSVData]
+    public partial class People 
+    {
+        public string Name { set; get; }
+        public int Age { set; get; }
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.Class)]
+    public class CSVDataAttribute:Attribute
+    {
+        //public string Name {  get; set; }
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.Property)]
+    public class CsvIgnoreAttribute : Attribute
+    {
+
     }
 }
--- QSoft.Ini/ToString.cs
+++ QSoft.Ini/ToString.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 
+//https://www.cnblogs.com/wanghun315/p/17236982.html
 namespace QSoft.Ini
 {
     [Generator]
@@ -12,21 +14,58 @@ namespace QSoft.Ini
     {
         public void Execute(GeneratorExecutionContext context)
         {
-            context.AddSource("Example.g.cs",@"
-public static class Example {
-    	public static int Value = 42;
-}");
+            AugmentSyntaxReceiver syntaxReceiver = (AugmentSyntaxReceiver)context.SyntaxReceiver;
+
+            // 通过语法接收器获得class类
+            ClassDeclarationSyntax augmentClass = syntaxReceiver.ClassToAugment;
+            //判断是否有这个类
+            if (augmentClass is null)
+            {
+                //没有找到就不做任何事情
+                return;
+            }
+
+            //找到了就添加一个方法
+            SourceText sourceText = SourceText.From($@"
+namespace {syntaxReceiver.SpaceToAugment.Name.GetText()}
+{{
+    public partial class {augmentClass.Identifier}
+    {{
+        public void GeneratedMethod()
+        {{
+            Console.WriteLine(""Hello, augmentClass!"");
+            Console.WriteLine(""{ DateTime.Now}"");
+        }}
+    }}
+}}", Encoding.UTF8);
+            context.AddSource("augmentClass.Generated.cs", sourceText);
+
+            //            context.AddSource("Example.g.cs",@"
+            //public static class Example {
+            //    	public static int Value = 42;
+            //}");
         }
 
         public void Initialize(GeneratorInitializationContext context)
         {
-//#if DEBUG
-//            if (!Debugger.IsAttached)
-//            {
-//                Debugger.Launch();
-//            }
-//#endif 
-            //Debug.WriteLine("Initalize code generator");
+#if DEBUG
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }
+#endif
+
+            //context.RegisterForSyntaxNotifications(() => new AugmentSyntaxReceiver());
+            context.RegisterForSyntaxNotifications(() => new CsvSyntaxReceiver());
+        }
+    }
+
+    public class CsvSyntaxReceiver : ISyntaxReceiver
+    {
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            System.Diagnostics.Trace.WriteLine($"{syntaxNode.GetType().Name}");
+            var cds = syntaxNode as ClassDeclarationSyntax;
         }
     }
 
